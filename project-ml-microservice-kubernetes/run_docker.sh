b4f6c3dbbf968ffa1ab8e3297c61cc7b99187299
dogbern <dogbern@gmail.com>
2020-06-02 05:22:13 -0500

    Project for machine learning microservice with kubernetes

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,13 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=predict_app .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+# publish it using -p HOST:CONTAINER
+docker run -p 8000:80 predict_app
--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=dogbern/predict_app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run predict-app\
+    --image=$dockerpath\
+    --port=80 --labels app=predict-app
 
 # Step 3:
 # List kubernetes pods
+kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward predict-app 8000:80
